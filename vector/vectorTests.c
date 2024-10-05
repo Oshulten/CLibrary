291fff3bc718befb6249d7156773d5702cb850cd
Oscar Hultén <oshulten@gmail.com>
2024-10-05 22:29:36 +0200

    feat: add vector creation test

--- vector/vectorTests.c
+++ vector/vectorTests.c
@@ -7,8 +7,40 @@
 #include "vector.h"
 #include "vectorTests.h"
 
+#include <float.h>
 #include <stdio.h>
 
+bool almostEqual(const double value, const double comparisonValue, const double epsilon) {
+    return abs(value - comparisonValue) <= epsilon;
+}
+
+bool arraysAreEqual(int count, double array[count], double comparison[count]) {
+    for (int i = 0; i < count; i++) {
+        if (!(almostEqual(array[i], comparison[i], DBL_MIN)))
+            return false;
+    }
+    return true;
+}
+
+void shouldCreateVectorWithCorrectElementsAndDimension() {
+    printf("shouldCreateVectorWithCorrectElementsAndDimension\n");
+    char *testTitles[] = { "Elements are correct", "Dimension is correct" };
+
+    Vector v = createVector(3, 1.0, 2.0, 3.0);
+    double comparison[] = { 1.0, 2.0, 3.0 };
+
+    bool testResults[] = {
+        arraysAreEqual(3, comparison, v.elements),
+        v.dimension == 3
+    };
+
+    for (int i = 0; i < 2; i++) {
+        printf("%s: %s\n", testTitles[i], testResults[i] ? "Pass" : "Fail");
+    }
+
+    printVector(v);
+}
+
 void runVectorTests() {
-    printf("Vector tests");
+    shouldCreateVectorWithCorrectElementsAndDimension();
 }
