d048b63a52b5c62aec5c5084bdb2cd3d6b454098
Oscar Hultén <oshulten@gmail.com>
2024-10-06 23:41:28 +0200

    feat: add test suite results

--- vector/vectorTests.c
+++ vector/vectorTests.c
@@ -21,7 +21,7 @@ void printTest(char *testTitle, const int testCount, const char* testTitles[], c
     printf("Result: %d/%d\n\n", passingTests, testCount);
 }
 
-void testCreateVector(const int dimension, double elements[]) {
+bool testCreateVector(const int dimension, double elements[]) {
     char *testTitles[] = { "Elements are correct", "Dimension is correct" };
 
     Vector v = createVector(dimension, elements);
@@ -32,10 +32,13 @@ void testCreateVector(const int dimension, double elements[]) {
     };
 
     printTest("createVector", 2, testTitles, testResults);
+
     freeVector(v);
+
+    return testResults[0] && testResults[1];
 }
 
-void testVectorsAreEqual(const Vector firstVector, const Vector secondVector, const bool equals) {
+bool testVectorsAreEqual(const Vector firstVector, const Vector secondVector, const bool equals) {
     char *testTitles[] = { "Vectors are equal" };
 
     const bool testResults[] = {
@@ -43,11 +46,14 @@ void testVectorsAreEqual(const Vector firstVector, const Vector secondVector, co
     };
 
     printTest("vectorsAreEqual", 1, testTitles, testResults);
+
     freeVector(firstVector);
     freeVector(secondVector);
+
+    return testResults[0];
 }
 
-void testCopyVector(const Vector vector) {
+bool testCopyVector(const Vector vector) {
     char *testTitles[] = { "Vectors are equal" };
 
     const Vector copiedVector = copyVector(vector);
@@ -56,10 +62,13 @@ void testCopyVector(const Vector vector) {
     };
 
     printTest("copyVector", 1, testTitles, testResults);
+
     freeVector(copiedVector);
+
+    return testResults[0];
 }
 
-void testTranslateVector(const Vector vector, const Vector translation, const Vector result) {
+bool testTranslateVector(const Vector vector, const Vector translation, const Vector result) {
     char *testTitles[] = { "Vectors are equal" };
 
     const bool testResults[] = {
@@ -69,44 +78,59 @@ void testTranslateVector(const Vector vector, const Vector translation, const Ve
     };
 
     printTest("translateVector", 1, testTitles, testResults);
+
+    return testResults[0];
 }
 
 void runVectorTests() {
-    testCreateVector(4, (double[]){1.0, 4.1, 3.0, 4.0});
-    testCreateVector(3, (double[]){1.0, 2.0, 3.0});
-    testCreateVector(2, (double[]){1.0, 2.0});
-    testCreateVector(1, (double[]){0.0});
-    testCreateVector(0, (double[]){});
+    printf("\nVector Tests\n-----\n");
 
-    testVectorsAreEqual(
-        createVector(2, (double[]){0.0, 0.0}),
-        createVector(2, (double[]){0.0, 0.0}),
-        true);
-    testVectorsAreEqual(
-        createVector(3, (double[]){0.0, 0.0, 0.0}),
-        createVector(2, (double[]){0.0, 0.0}),
-        false);
-    testVectorsAreEqual(
-        createVector(3, (double[]){0.0, 0.0, 0.0}),
-        createVector(3, (double[]){0.0, 0.0, 1.0}),
-        false);
+    const bool testResults[] = {
+        testCreateVector(4, (double[]){1.0, 4.1, 3.0, 4.0}),
+        testCreateVector(3, (double[]){1.0, 2.0, 3.0}),
+        testCreateVector(2, (double[]){1.0, 2.0}),
+        testCreateVector(1, (double[]){0.0}),
+        testCreateVector(0, (double[]){}),
 
-    testCopyVector(createVector(0, (double[]){}));
-    testCopyVector(createVector(1, (double[]){0.0}));
-    testCopyVector(createVector(2, (double[]){0.0, 0.0}));
+        testVectorsAreEqual(
+            createVector(2, (double[]){0.0, 0.0}),
+            createVector(2, (double[]){0.0, 0.0}),
+            true),
+        testVectorsAreEqual(
+            createVector(3, (double[]){0.0, 0.0, 0.0}),
+            createVector(2, (double[]){0.0, 0.0}),
+            false),
+        testVectorsAreEqual(
+            createVector(3, (double[]){0.0, 0.0, 0.0}),
+            createVector(3, (double[]){0.0, 0.0, 1.0}),
+            false),
 
-    testTranslateVector(
-        createVector(2, (double[]){0.0, 0.0}),
-        createVector(2, (double[]){0.0, 0.0}),
-        createVector(2, (double[]){0.0, 0.0}));
+        testCopyVector(createVector(0, (double[]){})),
+        testCopyVector(createVector(1, (double[]){0.0})),
+        testCopyVector(createVector(2, (double[]){0.0, 0.0})),
 
-    testTranslateVector(
-        createVector(3, (double[]){0.0, 0.0, 0.0}),
-        createVector(1, (double[]){1.0}),
-        createVector(3, (double[]){1.0, 1.0, 1.0}));
+        testTranslateVector(
+            createVector(2, (double[]){0.0, 0.0}),
+            createVector(2, (double[]){0.0, 0.0}),
+            createVector(2, (double[]){0.0, 0.0})),
 
-    testTranslateVector(
-        createVector(3, (double[]){0.0, 0.0, 0.0}),
-        createVector(5, (double[]){1.0, 1.0, 1.0, 1.0, 1.0}),
-        createVector(3, (double[]){1.0, 1.0, 1.0}));
+        testTranslateVector(
+            createVector(3, (double[]){0.0, 0.0, 0.0}),
+            createVector(1, (double[]){1.0}),
+            createVector(3, (double[]){1.0, 1.0, 1.0})),
+
+        testTranslateVector(
+            createVector(3, (double[]){0.0, 0.0, 0.0}),
+            createVector(5, (double[]){1.0, 1.0, 1.0, 1.0, 1.0}),
+            createVector(3, (double[]){1.0, 1.0, 1.0}))
+    };
+
+    size_t numberOfTests = sizeof(testResults) / sizeof(bool);
+
+    size_t positiveResults = 0;
+    for (int i = 0; i < numberOfTests; i++)
+        positiveResults += testResults[i];
+
+    printf("\n-----\nVector Tests Results: %llu/%llu\n-----", positiveResults, numberOfTests);
+
 }
